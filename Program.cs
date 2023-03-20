a16c8890f9b74e2b3803f8957b28416ab5c20b9b
MansiRavindraMohan <manasimohan29@gmail.com>
2023-03-20 23:19:16 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeePayrollProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Problem");
+        }
+    }
+}
